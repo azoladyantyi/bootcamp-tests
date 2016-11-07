7f0922d9411f04c295e95c41c46a3aa8ada0b6fc
azola <aizydyantyi@gmail.com>
2016-11-07 09:49:31 +0200

    initial commit

---
+++ yolo.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+{
+  var dayDetails = function('')
+  isWeekday = string.endsWith('day');
+  isWeekday = string.startsWith('S');
+  console.log(isWeekday);
+  console.log(isWeekday);
+}
+assert.equal(isWeekday('January'), false);
