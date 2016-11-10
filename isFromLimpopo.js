31e8dfa353fe2a6f6e0471c180938f5a43bbcdd5
azola <aizydyantyi@gmail.com>
2016-11-10 15:54:10 +0200

    initial commit

--- isFromLimpopo.js
+++ isFromLimpopo.js
@@ -1,6 +1,7 @@
 const assert = require ('assert');
 var isFromLimpompo = function(regNumber){
-  var results = regNumber.startsWith('DRT 122 L');
+var results = regNumber.startsWith('DRT 122 L');
+console.log(results);
   return results;
 };
 assert.equal(isFromLimpompo('DRT 122 L GP 100 '), true);
