a9700661823899be6be3c7f609e988c2c5ecd13f
azola <aizydyantyi@gmail.com>
2016-11-10 15:52:35 +0200

    initial commit

---
+++ isWeekday.js
@@ -0,0 +1,9 @@
+const assert = require ('assert');
+var isWeekday = function(day){
+  var results = !day.startsWith('S');
+  console.log(results);
+  return results;
+};
+assert.equal(isWeekday('Tuesday'), true);
+assert.equal(isWeekday('Sunday'),false);
+assert.equal(isWeekday('Sunday'),false);
