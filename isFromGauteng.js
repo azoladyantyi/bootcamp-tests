25739e921a0614a45f0919c8d5b8324c2effa36d
azola <aizydyantyi@gmail.com>
2016-11-11 10:21:06 +0200

    initial commit

--- isFromGauteng.js
+++ isFromGauteng.js
@@ -1,6 +1,7 @@
 const assert = require ('assert');
 var isFromGauteng = function(regNumber){
   var results = regNumber.startsWith('DR 12 TY GP');
+console.log(results);
   return results;
 };
 assert.equal(isFromGauteng('DR 12 TY GP 100 '), true);
--- isFromPaarl.js
+++ isFromPaarl.js
@@ -1,6 +1,7 @@
 const assert = require ('assert');
 var isFromPaarl = function(regNumber){
   var results = regNumber.startsWith('CJ');
+console.log(results);
   return results;
 };
 assert.equal(isFromPaarl('CJ 512'), true);
