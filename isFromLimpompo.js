1d7b85a1287cb3233e76227882431ebab7994a0e
azola <aizydyantyi@gmail.com>
2016-11-11 09:12:22 +0200

    i dont need this file anymore

--- isFromLimpompo.js
+++
@@ -1,6 +0,0 @@
-const assert = require ('assert');
-var isFromLimpompo = function(regNumber){
-  var results = regNumber.startsWith('DRT 122 L');
-  return results;
-};
-assert.equal(isFromLimpompo('DRT 122 L GP 100 '), true);
