01e5f8f83233079610ac2d2fe44552d3b76067d3
azola <aizydyantyi@gmail.com>
2016-11-11 10:23:31 +0200

    initial commit

--- isFromCapeTown.js
+++ isFromCapeTown.js
@@ -1,6 +1,7 @@
 const assert = require ('assert');
 var isFromCapeTown = function(regNumber){
   var results = regNumber.startsWith('CA');
+  console.log(results);
   return results;
 };
 assert.equal(isFromCapeTown('CA 23'), true);
