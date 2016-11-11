f6cba730191f4ef80f3aedf36a5d00653b7310ca
azola <aizydyantyi@gmail.com>
2016-11-11 08:34:01 +0200

    initial commit

--- countRegNumber.js
+++ countRegNumber.js
@@ -1,6 +1,7 @@
 const assert = require ('assert');
 var countRegNumber = function(registrationNumber){
   var results =registrationNumber.split(',')
+  console.log(results);
   return results.length;
 };
 assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328'),3);
