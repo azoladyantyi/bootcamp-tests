6ff6c65c7d57afe69d3a9d87ba86a75129544a46
azola <aizydyantyi@gmail.com>
2016-11-14 16:38:12 +0200

    my own file

--- yolo.js
+++ yolo.js
@@ -1,9 +1,7 @@
-const assert = require('assert');
-{
-  var dayDetails = function('')
-  isWeekday = string.endsWith('day');
-  isWeekday = string.startsWith('S');
-  console.log(isWeekday);
-  console.log(isWeekday);
-}
-assert.equal(isWeekday('January'), false);
+var checkMonth = function(month){
+  var date = new Date(month);
+  var m = date.getMonth();
+  console.log(m);
+  return m;
+};
+checkMonth('june, 6, 2016');
