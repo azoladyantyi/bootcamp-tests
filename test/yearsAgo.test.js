9bc8d8556ae4e2010b064d2e49b73849041587ee
azoladyantyi <aizydyantyi@gmail.com>
2017-05-09 13:53:04 +0200

    commit the changes that were made

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,10 +1,6 @@
 describe('yearsAgo', function(){
-    it("should return '6' when given 'year.getFullYear() - date.getFullYear()' ", function(){
-    assert.equal(yearsAgo('2016-01-25'),6);
-
-    });
-    it("should return '-3' when given 'year.getFullYear() - date.getFullYear()' ", function(){
-    assert.equal(yearsAgo('2007-01-25'),-3);
-
-    });
+it("should return '22' when given 'today.getFullYear()'", function() {
+assert.equal(yearsAgo(1995), '22');
 });
+});
+
--- yearsAgo.js
+++ yearsAgo.js
@@ -1,9 +1,7 @@
-//const assert = require ('assert')
-const date = new Date('2010-04-01');
-var yearsAgo = function(currentYear){
-  var year = new Date(currentYear);
-  var results = year.getFullYear() - date.getFullYear();
-  console.log(results);
-  return results;
-};
-//assert.equal(yearsAgo('2016-01-25'),6);
+//const assert = require('assert');
+function yearsAgo(year){
+var today = new Date();
+return today.getFullYear() - year;
+}
+console.log(yearsAgo(1995));
+//assert.equal(yearsAgo(1995), '22');
