c042fc0528eb8df46617a229689b9d14094bcd65
azoladyantyi <aizydyantyi@gmail.com>
2017-05-09 13:59:31 +0200

    commit the changes on a file

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -2,5 +2,8 @@ describe('yearsAgo', function(){
 it("should return '22' when given 'today.getFullYear()'", function() {
 assert.equal(yearsAgo(1995), '22');
 });
+it("should return '30' when given 'today.getFullYear()'", function() {
+assert.equal(yearsAgo(1987), '30');
+});
 });
 
